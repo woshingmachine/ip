c8d4eccbc3b4eb2d7289b5a46b054001185fa94c
woshingmachine <134035634+woshingmachine@users.noreply.github.com>
2026-01-21 18:06:29 +0800

    Completed Level-0 (renamed Duke to Bluga)

---
+++ src/main/java/Bluga.java
@@ -0,0 +1,21 @@
+public class Bluga {
+    public static void main(String[] args) {
+        String name = "______ _                   \n" +
+                "| ___ \\ |                  \n" +
+                "| |_/ / |_   _  __ _  __ _ \n" +
+                "| ___ \\ | | | |/ _` |/ _` |\n" +
+                "| |_/ / | |_| | (_| | (_| |\n" +
+                "\\____/|_|\\__,_|\\__, |\\__,_|\n" +
+                "                __/ |      \n" +
+                "               |___/       \n";
+        String lines = "------------------------------\n";
+        System.out.println(
+                lines
+                + "Hello, I'm\n"
+                + name
+                + lines
+                + "What can I do for you?\n"
+                + lines
+                + "Bye bye!");
+    }
+}
