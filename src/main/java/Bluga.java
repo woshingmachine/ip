1c40f4c10ad4c8670dcad70df9d5cb171f46b739
woshingmachine <134035634+woshingmachine@users.noreply.github.com>
2026-01-21 18:47:07 +0800

    Completed Level-2 (Added listing ability to Bluga)

--- src/main/java/Bluga.java
+++ src/main/java/Bluga.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bluga {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         String name = "______ _                   \n" +
                 "| ___ \\ |                  \n" +
                 "| |_/ / |_   _  __ _  __ _ \n" +
@@ -25,8 +27,13 @@ public class Bluga {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(lines + "    Bye!\n" + lines);
                 break;
+            } else if (input.equalsIgnoreCase("list")){
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i + 1 + ". " + list.get(i) + "\n");
+                }
             } else {
-                System.out.println(lines + "    " + input + "\n" + lines);
+                list.add(input);
+                System.out.println(lines + "    Added: " + input + "\n" + lines);
             }
         }
     }
